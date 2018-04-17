5eb4da7ea9473b8741c491f5cdbf03a854692577
mariekhater <36771771+mariekhater@users.noreply.github.com>
2018-04-17 22:09:18 +0200

    Update bubble.js

--- js/bubble.js
+++ js/bubble.js
@@ -60,7 +60,11 @@ function mouseOver(d){
 		}
 
 function color_filling(d){
+	if (d.data.auteur == "Saint-Mars, Dominique de" || d.data.auteur == "Saint Mars, Dominique de"){
+		return hsl(0, 100%, 50%);
+	} else {
 	return d.children ? color(d.depth) : null;
+	}
 }
 
 function mouseOut(){
