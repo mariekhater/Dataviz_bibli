fd5915c04b52e99eb90fba02342ea3cbc6822712
mariekhater <36771771+mariekhater@users.noreply.github.com>
2018-04-17 22:34:51 +0200

    Update bubble.js

--- js/bubble.js
+++ js/bubble.js
@@ -64,7 +64,7 @@ function mouseOver(d){
 function color_filling(d){
 	if (d.data.auteur == "Saint-Mars, Dominique de" || d.data.auteur == "Saint Mars, Dominique de" || d.data.auteur == "Vargas, Fred"){
 		return "hsl(0, 100%, 50%)";
-	} else if ((d.data.auteur == "Musso, Guillaume"){
+	} else if (d.data.auteur == "Musso, Guillaume"){
 		return "hsl(120, 100%, 50%)";
 	} else {
 	return d.children ? color(d.depth) : null;
