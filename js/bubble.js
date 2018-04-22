676d61d18bb6f9862d1294448a80f7f01bf5c43f
mariekhater <36771771+mariekhater@users.noreply.github.com>
2018-04-22 10:54:45 +0200

    Update bubble.js

--- js/bubble.js
+++ js/bubble.js
@@ -5,7 +5,7 @@ var svg = d3.select("#bubble"),
 
 var color = d3.scaleLinear()
     .domain([-1, 3])
-    .range(["hsl(190,100%,80%)", "hsl(190,100%,20%)"])
+    .range(["hsl(190,100%,100%)", "hsl(190,100%,20%)"])
     .interpolate(d3.interpolateHcl);
 
 var pack = d3.pack()
