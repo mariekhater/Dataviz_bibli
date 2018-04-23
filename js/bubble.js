d54313f5e4b064de7ef736d226a77ba99b9b8ea7
mariekhater <36771771+mariekhater@users.noreply.github.com>
2018-04-23 15:27:17 +0200

    Update bubble.js

--- js/bubble.js
+++ js/bubble.js
@@ -45,8 +45,8 @@ function mouseOver(d){
 			d3.select("#tooltip").transition().duration(200).style("opacity", .9);      
 			
 			d3.select("#tooltip").html(tooltip(d))  
-				.style("left", (44 + "px")) 
-				.style("top", (447 + "px"));
+				.style("left", (80 + "px")) 
+				.style("top", (500 + "px"));
 				//.style("left", (d.x + "px"))     
 				//.style("top", (d.y + "px"));
 		}
